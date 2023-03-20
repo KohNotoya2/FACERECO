82828c67d16591cfa5f24864d57916d97e2e16cd
KohNotoya2 <93630361+KohNotoya2@users.noreply.github.com>
2023-03-20 18:36:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+	width:350,height:300,image_format:"png",png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function takeSnapshot() {
+	Webcam.snap(function(data_uri) {
+		document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+	});
+}
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/AgctFboXQ/model.json",modelLoaded);
+
+function modelLoaded() {
+	console.log("MODEL LOADED");
+}
+
+function check() {
+	img=document.getElementById("captured_image");
+	classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results) {
+	if(error) {
+		console.log(error);
+	}
+
+	else {
+		console.log(results);
+		document.getElementById("identified").innerHTML=results[0].label;
+		document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(2);
+	}
+}
